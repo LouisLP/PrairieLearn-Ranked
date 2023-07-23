e226417c1d71a5b7f1d3a149e2204b613ac78206
LouisLP <louis.lascelles.palys@gmail.com>
2023-07-23 15:14:07 -0700

    Adjusted query to be more verbose

--- apps/prairielearn/src/pages/plrStudent/plrStudent.sql
+++ apps/prairielearn/src/pages/plrStudent/plrStudent.sql
@@ -3,8 +3,8 @@ SELECT
    display_name,
    points
 FROM
-   PLR_seasonal_session_CREDENTIALS
-   JOIN PLR_students USING (user_id)
+   PLR_seasonal_session_credentials
+   INNER JOIN PLR_students ON PLR_seasonal_session_credentials.user_id = PLR_students.user_id
 ORDER BY
    points DESC;
 -- ENDBLOCK
