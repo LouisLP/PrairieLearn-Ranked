899b0171244234dfd5f54f5a74e7a6ce3eeb9599
LouisLP <louis.lascelles.palys@gmail.com>
2023-07-25 09:57:02 -0700

    Updated comments/formatting

--- apps/prairielearn/src/admin_queries/PLR_end.sql
+++ apps/prairielearn/src/admin_queries/PLR_end.sql
@@ -1,8 +1,9 @@
--- Stop the live session and return the updated rows
+-- Stop the live session
 UPDATE
   PLR_live_session
 SET
   is_live = FALSE
 WHERE
   is_live = TRUE 
+-- Return the updated rows
 RETURNING *;
