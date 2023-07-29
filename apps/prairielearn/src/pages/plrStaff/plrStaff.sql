a82203250a98cc07436e45ded53b515f045be72f
LouisLP <louis.lascelles.palys@gmail.com>
2023-07-29 14:38:11 -0700

    Query to get titles with "LV" tag

--- apps/prairielearn/src/pages/plrStaff/plrStaff.sql
+++ apps/prairielearn/src/pages/plrStaff/plrStaff.sql
@@ -1,1 +1,13 @@
--- TODO: Query to get available quizzes
+-- BLOCK get_quizzes
+SELECT
+   assessments.title,
+   assessments.id,
+   assessments.course_instance_id
+FROM
+   assessments
+   JOIN assessment_sets ON assessments.assessment_set_id = assessment_sets.id
+WHERE
+   assessment_sets.abbreviation = 'LV'
+   AND assessments.course_instance_id = $1;
+
+-- ENDBLOCK
