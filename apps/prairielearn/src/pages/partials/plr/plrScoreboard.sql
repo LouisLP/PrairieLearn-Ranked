d2872942b95112a8ab30f0a1118c9e53b8498ad1
Rylan Cox <rylanrcox@gmail.com>
2023-07-29 17:05:59 -0700

    Fix seasonal scoreboard and add all time

--- apps/prairielearn/src/pages/partials/plr/plrScoreboard.sql
+++ apps/prairielearn/src/pages/partials/plr/plrScoreboard.sql
@@ -3,7 +3,21 @@ SELECT
    display_name,
    points
 FROM
-   PLR_live_session_CREDENTIALS
+   PLR_seasonal_session_CREDENTIALS
+   JOIN PLR_students USING (user_id)
+   JOIN PLR_seasonal_session on session_id = PLR_seasonal_session.id
+WHERE
+   course_instance_id = $1
+ORDER BY
+   points DESC;
+-- ENDBLOCK
+
+-- BLOCK get_alltime_results
+SELECT
+   display_name,
+   points
+FROM
+   PLR_seasonal_session_CREDENTIALS
    JOIN PLR_students USING (user_id)
 WHERE
    course_instance_id = $1
