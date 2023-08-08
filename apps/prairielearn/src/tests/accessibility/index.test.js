bc248af9328f2fc3df36108ff2c3d6b53a8fea20
LouisLP <louis.lascelles.palys@gmail.com>
2023-08-08 15:22:59 -0700

    Pushing to see logs

--- apps/prairielearn/src/tests/accessibility/index.test.js
+++ apps/prairielearn/src/tests/accessibility/index.test.js
@@ -57,6 +57,7 @@ function cleanLargePages(url, page) {
  * @returns {Promise<import('jsdom').JSDOM>}
  */
 async function loadPageJsdom(url) {
+  console.log(url);
   const text = await fetch(url).then((res) => {
     if (!res.ok) {
       throw new Error(`Error loading page: ${res.status}`);
